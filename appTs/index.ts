921da9fff2327d93a101f84c16b70dd06c3287a8
robson-alessandro <robsonalessandro74@gmail.com>
2024-10-09 09:45:12 -0300

    primeiro commit

---
+++ appTs/index.ts
@@ -0,0 +1,7 @@
+const form = <HTMLFormElement>document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const formDate = new FormData(form);
+	const data = Object.fromEntries(formDate);
+});
